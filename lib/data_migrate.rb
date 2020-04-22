8ce84767beb753cbb9629945ae53f50757b6ddf8
igalPR <igalpr2002@gmail.com>
2020-04-22 15:48:18 -0300

    1.1

--- lib/data_migrate.rb
+++
@@ -1,8 +0,0 @@
-require File.join(File.dirname(__FILE__), 'data_migrate', 'data_migrator')
-require File.join(File.dirname(__FILE__), 'data_migrate', 'data_schema_migration')
-if Rails::VERSION::MAJOR >= 5
-  require File.join(File.dirname(__FILE__), 'data_migrate', 'migration_five')
-else
-  require File.join(File.dirname(__FILE__), 'data_migrate', 'migration')
-end
-require File.join(File.dirname(__FILE__), 'data_migrate', 'railtie')
